c59fd88fd07f8958fd53a65ecd64d4a9d8c96f4e
PRATIK DWIVEDI <pratikdwivedi107@gmail.com>
2026-03-23 00:20:45 +0530

    Update LoginModal.tsx

--- components/auth/LoginModal.tsx
+++ components/auth/LoginModal.tsx
@@ -17,7 +17,7 @@ export default function LoginModal({ onClose }: { onClose: () => void }) {
     
     setTimeout(() => {
       localStorage.setItem('userType', userType)
-      localStorage.setItem('userName', email.split('@'))
+      localStorage.setItem('userName', email.split('@')[0])
       router.push(`/${userType}`)
       setLoading(false)
     }, 800)
